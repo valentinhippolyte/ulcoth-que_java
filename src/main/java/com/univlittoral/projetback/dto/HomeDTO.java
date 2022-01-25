edae0c3932812750e01ab69925c02766a68adb9f
valentin.hippolyte <valenthippo@gmail.com>
2022-01-25 09:54:00 +0100

    first-commit

---
+++ src/main/java/com/univlittoral/projetback/dto/HomeDTO.java
@@ -0,0 +1,35 @@
+package com.univlittoral.projetback.dto;
+
+import java.util.List;
+import java.util.Map;
+
+
+public class HomeDTO {
+
+    private List<LivreDTO> livres;
+    private IndicateursDTO indicateur;
+    private Map<String,Integer> genres;
+
+	
+	
+	public Map<String, Integer> getGenres() {
+		return genres;
+	}
+	public void setGenres(Map<String, Integer> genres) {
+		this.genres = genres;
+	}
+	public List<LivreDTO> getLivres() {
+		return livres;
+	}
+	public void setLivres(List<LivreDTO> livres) {
+		this.livres = livres;
+	}
+	public IndicateursDTO getIndicateurs() {
+		return indicateur;
+	}
+	public void setIndicateurs(IndicateursDTO indicateur) {
+		this.indicateur = indicateur;
+	}
+	
+	
+}
